6cd48e64689817d16ddb8aacffc95c9e07e76e23
Beatryz-Moura <beatryz_moura@hotmail.com>
2017-10-07 17:16:30 -0300

    APS projeto contaBancaria

---
+++ contaBancaria/src/contabancaria/Conta.java
@@ -0,0 +1,24 @@
+/*
+que possua um saldo os métodos para pegar saldo, depositar e sacar
+ */
+package contabancaria;
+
+public class Conta {
+    
+    private float saldo;
+
+    public float getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    public void depositar(){
+        
+    }
+    public void sacar(){
+
+    }    
+    
+    
+}
